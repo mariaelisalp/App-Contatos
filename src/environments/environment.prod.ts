be14197990cc80a07a21b56384f9911b6a2e46e5
mariaelisalp <mariaelisalima04@gmail.com>
2024-07-19 10:25:22 -0300

    Validação de formulário e hosting da aplicação

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyASq4yPVDXjXDOHPQAn_yECGE0tFpkS7q4",
+    authDomain: "appcontatos-62ad2.firebaseapp.com",
+    projectId: "appcontatos-62ad2",
+    storageBucket: "appcontatos-62ad2.appspot.com",
+    messagingSenderId: "337670928630",
+    appId: "1:337670928630:web:dddffb2e5b9fb511db94d7"
+  }
 };
